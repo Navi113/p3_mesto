70556a356dbd6aefd5af2b5d859d583e23e73bdd
Ivan Ershov <Navi133@yandex.ru>
2024-12-03 18:04:51 +0300

    Доделал попапы

--- script.js
+++ script.js
@@ -23,21 +23,43 @@ let buttonClsGallery = formGallery.querySelector('#button-close-photo-info');
 
 // Card
 let gallery = document.querySelector('.gallery');
-let card = gallery.querySelector('.card');
-let cardPhoto = card.querySelector('.card__photo');
-let cardInfo = card.querySelector('.card__info-container');
-let cardTitle = cardInfo.querySelector('.card__title');
-let buttonLike = cardInfo.querySelector('.card__like-button');
 
-function takeLike() {
-  buttonLike.classList.toggle('card__like-button_active');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// цикл перебирающий массив initialCards и вставляющий их в разметку
+for (let i = 0; i < initialCards.length; i++) {
+  addPhoto(initialCards[i].name, initialCards[i].link)
 }
-
-buttonLike.addEventListener('click', takeLike)
 
 //  слушатель на кнопку edit
 buttonEdit.addEventListener('click', function () {
   popupUserInfo.classList.add('popup_active'); // присвоил класс для открытия попапа
+  
   nameInput.value = userName.textContent; // содержимое имени профиля передал в форму
   jobInput.value = userJob.textContent; // содержимое описания профиля передал в форму
 });
@@ -52,6 +74,18 @@ buttonClsUserInfo.addEventListener('click', function () {
   popupUserInfo.classList.remove('popup_active');
 });
 
+// слушатель на кнопку закрыть попап добавления информации о пользователе
+buttonClsUserInfo.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
+  popupUserInfo.classList.remove('popup_active');
+});
+
+// слушатель на кнопку закрыть попап в форме добавления нового фото
+buttonClsGallery.addEventListener('click', function () {
+  popupAddPhoto.classList.remove('popup_active');
+});
+
 // ф-я отправки формы user-info
 function handleFormSubmitUserInfo(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
@@ -62,5 +96,36 @@ function handleFormSubmitUserInfo(evt) {
   popupUserInfo.classList.remove('popup_active');
 }
 
-// слушатель на кнопку submit
+// ф-я поставить лайк
+function takeLike() {
+  buttonLike.classList.toggle('card__like-button_active');
+}
+
+// ф-я добавить новое фото в галлерею
+function addPhoto(nameValue, photoValue) {
+  const template = document.querySelector('#template').content; // наши шаблон и его содержимое
+  const cardElement = template.querySelector('.card').cloneNode(true); // клонировали содержимое шаблона
+
+  cardElement.querySelector('.card__photo').src = photoValue; // вставили значение из формы в клонированную разметку
+  cardElement.querySelector('.card__title').textContent = nameValue; // вставили значение из формы в клонированную разметку
+  cardElement.querySelector('.card__like-button').addEventListener('click', function (evt) { // слушатель на кнопку лайка в клонированной разметке
+    evt.target.classList.toggle('card__like-button_active'); // смена класса у конкретной выбранной иконки лайка
+  });
+
+  nameValue.value = ''; //чистим поля формы
+  photoValue.value = '';
+
+  popupAddPhoto.classList.remove('popup_active'); // закрываем попап
+
+  gallery.prepend(cardElement); // добавляем разметку в DOM
+}
+
+// слушатель на форму попапа добавления информации о пользователе
 formUserInfo.addEventListener('submit', handleFormSubmitUserInfo);
+
+// слушатель форму попапа добаления фото
+formGallery.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+
+  addPhoto(formNamePhoto.value, formUrlPhoto.value)
+});
